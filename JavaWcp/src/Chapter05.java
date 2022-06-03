8afd99a6d66b9ab5cd07f74cfd46912df5709e37
kaduki-samei <kaduki0331@gmail.com>
2022-06-04 00:32:29 +0900

    For02

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,23 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		
+		long long1 = 123456789L;
+		double double1 = 12.34d;
+		boolean boolean1 = true;
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+		
+		Float float1 = 12.34f;
+		Integer int1 = 100;
+		Character character1 = 'あ';
+		System.out.println(float1);
+		System.out.println(int1);
+		System.out.println(character1);
+		
+		int array1[] = { 0, 1, 2, 3, 4 };
+		System.out.println(array1[3]);
+	}
+
+}
---
+++ JavaWcp/src/For01.java
@@ -0,0 +1,9 @@
+
+public class For01 {
+	public static void main(String[] args) {
+//		for文…条件がtrueの間繰り返し処理する。主に回数指定する場合に用いる。
+		for (int number = 1; number <= 5; number++) {
+			System.out.println("For01 = " + number);
+		}
+	}
+}
