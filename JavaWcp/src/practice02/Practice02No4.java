cc2b6c543f8ffbc7c9c46d1b0b7bf2e81d844966
kaduki-samei <kaduki0331@gmail.com>
2022-06-05 18:43:03 +0900

    練習問題

---
+++ JavaWcp/src/practice02/Practice02No4.java
@@ -0,0 +1,28 @@
+package practice02;
+
+//要素数20の配列を宣言し、要素に0から5ずつ加算して代入させる
+//[0]~[10]の値の奇数のみカンマ区切りで、[11]~[19]の値は偶数をカンマ区切りで表示させる
+
+public class Practice02No4 {
+	public static void main(String[] atgs) {
+		int array[] = new int[20];
+		for (int i = 1; i <20; i++) {
+			array[i] = array[(i - 1)] + 5;
+		}
+		
+		for (int index = 0; index <= 10; index++) {
+			if (index % 2 == 0) {
+				continue;
+			}
+			System.out.print(array[index] + ",");
+		}
+		for (int index = 11; index <= 19; index++) {
+			if (index % 2 == 0) {
+				System.out.print(array[index] + ",");
+			}
+		}
+	}
+}
+
+//できたんじゃない？
+//いいんだよね？
